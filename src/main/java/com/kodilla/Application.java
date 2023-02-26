8695e1f4001849e55d1a0264e14467b0effcd330
Barhwald <bylinabass@gmail.com>
2023-02-26 21:22:16 +0100

    Second commit

--- src/main/java/com/kodilla/Application.java
+++ src/main/java/com/kodilla/Application.java
@@ -7,7 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
+
         SpringApplication.run(Application.class, args);
+
+        Calculator myCalc = new Calculator();
+        System.out.println(myCalc.Sum(2,45));
+        System.out.println(myCalc.Subtract(67, 37));
     }
 
 }
--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -2,5 +2,12 @@ package com.kodilla;
 
 public class Calculator {
 
+    public int Sum(int a, int b) {
+        return a + b;
+    }
+
+    public int Subtract(int a, int b) {
+        return a - b;
+    }
 
 }
